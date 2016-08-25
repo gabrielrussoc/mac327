f8571bd4b1a525aad28169a174a76c6655537aad
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-08-24 23:25:51 -0300

    double trouble

--- aula-7/a.cpp
+++ aula-7/a.cpp
@@ -4,28 +4,32 @@
 using namespace std;
 
 typedef long long ll;
+#define double long double
 
 const int N = 100;
 double eps = 1e-9;
 int p[N], ps, t; 
-ll prime[N], pot[N], n, div, ans, at;
+ll prime[N], pot[N], n, div, ans, loc;
 
-void solve(ll k) {
-    for(int i = 0; i < ps; i++)
-        if(log(k) + log(prime[i]) <= log(n)+eps) {
-            pot[i]++;
-            at /= pot[i];
-            at *= pot[i]+1;
-            if(at >= div) {
-                if(at > div) ans = k*prime[i];
-                else ans = min(ans, k*prime[i]);
-                div = at;
+void solve(ll k, int q, int e) {
+    ll lq = 1;
+    for(ll i = 1; i <= e; i++) {
+        lq *= prime[q];
+        if(k <= n/lq) {
+            pot[q] = i;
+            loc *= i+1;
+            solve(k*lq, q+1, i);
+            loc /= i+1;
+            pot[q] = 0;
+        } else {
+            if(loc >= div) {
+                if(loc == div) ans = min(ans, k);
+                else ans = k;
+                div = loc;
             }
-            solve(k*prime[i]);
-            at /= pot[i]+1;
-            at *= pot[i];
-            pot[i]--;
+            return;
         }
+    }
 }
 
 int main() {
@@ -39,10 +43,9 @@ int main() {
     scanf("%d",&t);
     while(t--) {
         ans = LLONG_MAX;
-        div = at = 1;
+        div = loc = 1;
         scanf("%lld",&n);
-        solve(1);
-        if(ans == LLONG_MAX) ans = 1;
+        solve(1, 0, 62);
         printf("%lld %lld\n",ans,div);
     }
 
