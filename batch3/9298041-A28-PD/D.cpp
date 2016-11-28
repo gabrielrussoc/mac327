af3ba52b10e80e58414519de30c384413376c85c
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-11-28 14:50:12 -0200

    passou o D

--- batch3/9298041-A28-PD/D.cpp
+++ batch3/9298041-A28-PD/D.cpp
@@ -10,21 +10,21 @@ int memo[N][N][2], visi[N][N][2], lcp[N][N];
 int n;
 char s[N];
 
+bool can(int i, int j, int len) {
+    int k = lcp[i][j];
+    if(k >= len) return false;
+    return s[j+k] > s[i+k];
+}
+
 int dp(int len, int i, bool big) {
-    printf("%d %d %d\n",len,i,big);
     if(i == n) return 1;
+    if(i+len > n || s[i] == '0') return 0;
     int &me = memo[len][i][big];
     if(visi[len][i][big]) return me;
     visi[len][i][big] = 1; 
-    me = dp(len+1,i+1,true);
-    if(i + len > n || s[i] == '0') return me;
-    if(big) me = (me + dp(len,i+len,false)) % MOD;
-    else {
-        int j = lcp[i-len][i];
-        if(j == len || s[i-len+j] < s[i+j])
-           me = (me + dp(len, i+len, false)) % MOD; 
-    }
-    return me % MOD;
+    me = dp(len+1, i, true);
+    if(big || can(i-len, i, len)) me = (me + dp(len, i+len, false)) % MOD; 
+    return me;
 }
 
 int main() {
@@ -33,6 +33,6 @@ int main() {
     for(int i = n-1; i >= 0; i--)
         for(int j = n-1; j >= 0; j--)
             lcp[i][j] = (s[i] == s[j]) ? 1 + lcp[i+1][j+1] : 0;
-    printf("%d\n",dp(1,1,true));
+    printf("%d\n",dp(1,0,true));
 }
 
